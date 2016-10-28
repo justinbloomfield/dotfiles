515634f204035f006ad1acb1cb62766fb0151985
Justin Bloomfield <jbloomfield@live.com>
2016-10-28 16:33:03 +1100

    idek

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,6 +1,7 @@
 ;;(package-initialize)
 
 (add-to-list 'load-path "~/.emacs.d/custom")
+(setq twittering-use-master-pasword t)
 
 
 ;; fak u
@@ -12,7 +13,6 @@
 (load "ivy-init.el")
 (load "twittering-mode.el")
 (load "mu4e-init.el")
-(setq twittering-use-master-pasword t)
 
 ;; ui
 (load "ui.el")
@@ -40,17 +40,3 @@
 
 ;; Org mode shiz (for now)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (evil-mu4 mu4e-evil mu4e evil-mu4e ein-mumamo use-package sublime-themes smooth-scrolling powerline nlinum-relative markdown-preview-mode markdown-preview-eww magit lua-mode linum-relative haskell-mode eww-lnum evil-visual-mark-mode evil-leader counsel circe buffer-move all-the-icons))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
