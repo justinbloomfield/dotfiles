28dbff51aeea8d9fde6bc9ba0bbc9255ebbe04d6
Justin Bloomfield <jbloomfield@live.com>
2017-02-09 22:20:01 +1100

    nogui actually added

---
+++ emacs/.emacs.d/custom/modeline-custom-nogui.el
@@ -0,0 +1,73 @@
+;; what's in it
+(defun custom-modeline-mode-icon ()
+  (let ((icon (all-the-icons-icon-for-buffer)))
+    (unless (symbolp icon) ;; This implies it's the major mode
+      (format " %s"
+            (propertize
+            icon
+            'face
+            `(:height 1.0 :family ,(all-the-icons-icon-family-for-buffer))
+            'display '(raise -0.1))))))
+
+(defun evil-mode-state ()
+  (let ((str (cond ((evil-normal-state-p) " n ")
+                   ((evil-insert-state-p) " i ")
+                   ((evil-visual-state-p) " v ")
+                   ((evil-emacs-state-p)  " e ")
+                   ((evil-operator-state-p) " o ")
+                   ((evil-motion-state-p) " m ")
+                   ((evil-replace-state-p) " r "))))))
+
+(defun vc-mode-branch-state ()
+  (let ((bufname (buffer-file-name (current-buffer))))
+    (when bufname
+      (when (magit-get-current-branch)
+        (propertize
+         (format " b: %s " (magit-get-current-branch)))))))
+         
+
+(defun time ()
+  (propertize (format-time-string " %H:%M ")))
+
+(defun current-buffer-title ()
+  (propertize " %b "))
+
+(defun blank ()
+  (propertize ""))
+
+(defun now-playing ()
+  (propertize
+   (format " %s " (shell-command-to-string "echo -n $(mpc current)")) ;; needs to be echo -n to avoid trailing ^J in output
+   )) 
+
+(defvar ml-selected-window nil)  
+
+(defun ml-record-selected-window ()  
+  (setq ml-selected-window (selected-window)))  
+  
+(defun ml-update-all ()  
+  (force-mode-line-update t))  
+  
+(add-hook 'post-command-hook 'ml-record-selected-window)  
+  
+(add-hook 'buffer-list-update-hook 'ml-update-all)  
+  
+(setq-default mode-line-format  
+  '(:eval  
+      (if (eq ml-selected-window (selected-window))  
+          (list
+           '(:eval (concat
+                   (custom-modeline-mode-icon)
+                   (current-buffer-title)
+                   (evil-mode-state)
+                   (vc-mode-branch-state)
+                   (blank)
+                   (now-playing)
+                   (time))))
+        (list
+         '(:eval (concat
+                  (custom-modeline-mode-icon)
+                  (current-buffer-title)
+                  (evil-mode-state)
+                  (vc-mode-branch-state)))))))
+ 
