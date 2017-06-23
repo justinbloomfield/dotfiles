4b4e5ad7e93ec7b1533b3a17462de288b05cd61c
Justin Bloomfield <jbloomfield@live.com>
2017-06-24 00:14:57 +1000

    mksh changes

---
+++ mksh/.mksh.d/env.sh
@@ -0,0 +1,17 @@
+EDITOR="emacs"
+PAGER="less"
+LANG=en_AU.UTF-8
+LC_CTYPE="en_AU.UTF-8"
+NTFY_FIFO_PATH="/home/$(whoami)/var/ntfy/ntfy.fifo"
+SRC_DIR="/home/$(whoami)/src/pkg"
+CHOME="/home/$(whoami)/usr"
+#PATH=/home/poq/bin/:/home/poq/.cabal/bin/:/usr/local/bin/:/usr/bin/:/sbin:/Users/j/bin:/opt/pkg/bin:/Library/TeX/texbin:/usr/sbin:/home/poq/src/go/bin:/home/poq/.local/bin:/opt/local/bin:/opt/local/sbin:/Users/j/.local/bin:/opt/texlive/2016/bin/x86_64-linux:/bin:/home/poq/.stack/programs/x86_64-linux/ghc-ncurses6-8.0.2/bin
+#WM="windowchef"
+BROWSER="firefox"
+WP_DIR="/home/poq/usr/img/wp"
+GOPATH="/home/poq/src/go"
+GTK_IM_MODULE=ibus
+XMODIFIERS=@im=ibus
+QT_IM_MODULE=ibus
+PS1=$'[35m─┄ [38m$(pwd | sed "s,^$HOME,~," | rev | cut -d '/' -f1 | rev )[00m '
+export CSC EDITOR PAGER NTFY_FIFO_PATH LC_CTYPE PATH CHOME WM WP_DIR GOPATH TERM GTK_IM_MODULE XMODIFIERS QT_IM_MODULE PS1
